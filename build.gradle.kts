5b154c1b181841866742a9122d3afea3c4c963f7
lukegjpotter <lukegjpotter@gmail.com>
2023-07-27 23:48:20 +0100

    Step 5: Cucumber Tests are being output in ./gradle test.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,20 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     // Cucumber Libraries.
+    testImplementation(platform("io.cucumber:cucumber-bom:7.13.0"))
     testImplementation("io.cucumber:cucumber-java:7.13.0")
     testImplementation("io.cucumber:cucumber-junit:7.13.0")
     testImplementation("io.cucumber:cucumber-junit-platform-engine:7.13.0")
+    testImplementation("org.junit.platform:junit-platform-suite")
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    // Work around. Gradle does not include enough information to disambiguate between different examples and scenarios.
+    systemProperty("cucumber.junit-platform.naming-strategy", "long")
 }
