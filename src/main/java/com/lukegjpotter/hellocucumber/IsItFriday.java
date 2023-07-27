73b69f60b400fb2729e8bacb5297fd35ed84e50e
lukegjpotter <lukegjpotter@gmail.com>
2023-07-27 23:11:36 +0100

    Step 4: Adding Variable and Refactoring.

---
+++ src/main/java/com/lukegjpotter/hellocucumber/IsItFriday.java
@@ -0,0 +1,8 @@
+package com.lukegjpotter.hellocucumber;
+
+public class IsItFriday {
+
+    public static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
--- src/test/java/com/lukegjpotter/hellocucumber/StepDefinitions.java
+++ src/test/java/com/lukegjpotter/hellocucumber/StepDefinitions.java
@@ -20,6 +20,11 @@ public class StepDefinitions {
         today = "Friday";
     }
 
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_friday_yet() {
         actualAnswer = IsItFriday.isItFriday(today);
@@ -28,11 +33,5 @@ public class StepDefinitions {
     @Then("I should be told {string}")
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
-    }
-}
-
-class IsItFriday {
-    static String isItFriday(String today) {
-        return "Friday".equals(today) ? "TGIF" : "Nope";
     }
 }
